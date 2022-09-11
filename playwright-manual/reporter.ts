0c77c94025d6e75b5e8ba5b62dbaa42b70ebbeb0
shammlo <shamlooo@yahoo.com>
2022-09-11 21:57:10 +0300

    Custom reporter

---
+++ playwright-manual/reporter.ts
@@ -0,0 +1,20 @@
+import { Reporter } from "@playwright/test/reporter";
+
+class MyReporter implements Reporter {
+  onBegin(config, suite) {
+    console.log(`Starting the run with ${suite.allTests().length} tests`);
+  }
+
+  onTestBegin(test) {
+    console.log(`Starting test ${test.title}`);
+  }
+
+  onTestEnd(test, result) {
+    console.log(`Finished test ${test.title}: ${result.status}`);
+  }
+
+  onEnd(result) {
+    console.log(`Finished the run: ${result.status}`);
+  }
+}
+export default MyReporter;
