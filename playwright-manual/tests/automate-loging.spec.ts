bc209e9fd61591c46d83146ac17a59c5f59f02ae
shammlo <shamlooo@yahoo.com>
2022-09-13 21:24:43 +0300

    Automating login

--- playwright-manual/tests/automate-loging.spec.ts
+++ playwright-manual/tests/automate-loging.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from "@playwright/test";
-test.describe("Login", () => {
+
+test.describe.skip("Login", () => {
   test.use({
-    storageState: "auth-login.json",
+    storageState: "auth.json",
   });
 
   test.beforeEach(async ({ page }) => {
@@ -10,24 +11,63 @@ test.describe("Login", () => {
 
   test.skip("saving session", async ({ page, context }) => {
     // Go to https://parabank.parasoft.com/parabank/index.htm
-    // Click input[name="username"]
-    await page.locator('input[name="username"]').click();
-    // Fill input[name="username"]
-    await page.locator('input[name="username"]').fill("testing123");
-    // Click input[name="password"]
-    await page.locator('input[name="password"]').click();
-    // Fill input[name="password"]
-    await page.locator('input[name="password"]').fill("test123");
-    // Click text=Log In
-    await page.locator("text=Log In").click();
+    // Click text=Register
+    await page.locator("text=Register").click();
 
+    // Click input[name="customer\.firstName"]
+    await page.locator('input[name="customer\\.firstName"]').click();
+    // Fill input[name="customer\.firstName"]
+    await page.locator('input[name="customer\\.firstName"]').fill("testing");
+    // Press Tab
+    await page.locator('input[name="customer\\.firstName"]').press("Tab");
+    // Fill input[name="customer\.lastName"]
+    await page.locator('input[name="customer\\.lastName"]').fill("test");
+    // Press Tab
+    await page.locator('input[name="customer\\.lastName"]').press("Tab");
+    // Fill input[name="customer\.address\.street"]
+    await page.locator('input[name="customer\\.address\\.street"]').fill("test");
+    // Press Tab
+    await page.locator('input[name="customer\\.address\\.street"]').press("Tab");
+    // Fill input[name="customer\.address\.city"]
+    await page.locator('input[name="customer\\.address\\.city"]').fill("test");
+    // Press Tab
+    await page.locator('input[name="customer\\.address\\.city"]').press("Tab");
+    // Fill input[name="customer\.address\.state"]
+    await page.locator('input[name="customer\\.address\\.state"]').fill("test");
+    // Press Tab
+    await page.locator('input[name="customer\\.address\\.state"]').press("Tab");
+    // Fill input[name="customer\.address\.zipCode"]
+    await page.locator('input[name="customer\\.address\\.zipCode"]').fill("04001");
+    // Press Tab
+    await page.locator('input[name="customer\\.address\\.zipCode"]').press("Tab");
+    // Fill input[name="customer\.phoneNumber"]
+    await page.locator('input[name="customer\\.phoneNumber"]').fill("3429384234");
+    // Press Tab
+    await page.locator('input[name="customer\\.phoneNumber"]').press("Tab");
+    // Fill input[name="customer\.ssn"]
+    await page.locator('input[name="customer\\.ssn"]').fill("test");
+    // Press Tab
+    await page.locator('input[name="customer\\.ssn"]').press("Tab");
+    // Fill input[name="customer\.username"]
+    await page.locator('input[name="customer\\.username"]').fill("test1212");
+    // Press Tab
+    await page.locator('input[name="customer\\.username"]').press("Tab");
+    // Fill input[name="customer\.password"]
+    await page.locator('input[name="customer\\.password"]').fill("test1212");
+    // Press Tab
+    await page.locator('input[name="customer\\.password"]').press("Tab");
+    // Fill input[name="repeatedPassword"]
+    await page.locator('input[name="repeatedPassword"]').fill("test1212");
+    // Click input:has-text("Register")
+    await page.locator('input:has-text("Register")').click();
     await context.storageState({ path: "auth-login.json" });
-
-    await expect(page).toHaveURL("https://parabank.parasoft.com/parabank/overview.htm");
   });
+
   test("checking", async ({ page }) => {
     await page.locator("#leftPanel >> text=Bill Pay").click();
+    await page.pause();
   });
+
   test("checking 2", async ({ page }) => {
     await page.locator("text=Transfer Funds").first().click();
   });
